7ccbc9b80435affa62721ace89a1e51095c95a53
Manu Morante <manu@estadologico.com>
2022-04-25 19:56:05 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+export default function App() {
+  return (
+    <div className='App bg-neutral-200'>
+      <h1>Vite + React + Tailwindcss Template</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.jsx', './index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
